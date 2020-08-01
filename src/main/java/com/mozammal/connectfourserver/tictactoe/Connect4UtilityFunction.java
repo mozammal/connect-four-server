f7dcdbf64c2d107eeac06ba3330e53d10517af03
mozammal <mozammaltomal.1001@gmail.com>
2020-08-01 02:34:58 +0200

    add more than one depth for alpha beta pruning

--- src/main/java/com/mozammal/connectfourserver/tictactoe/Connect4UtilityFunction.java
+++ src/main/java/com/mozammal/connectfourserver/tictactoe/Connect4UtilityFunction.java
@@ -1,4 +1,3 @@
-/*
 package com.mozammal.connectfourserver.tictactoe;
 
 import com.mozammal.connectfourserver.model.Board;
@@ -117,12 +116,10 @@ public class Connect4UtilityFunction implements GameUtilityFunction {
           } else cntDiagonal++;
           if (cntDiagonal == 4) {
             // log.info("yes2 ");
-            */
-/*  Board board = new Board();
+            /*  Board board = new Board();
             board.setBoard(boards);
             board.printBoard();
-            log.info("yes2 ");*//*
-
+            log.info("yes2 ");*/
             return 2;
           }
         }
@@ -152,16 +149,13 @@ public class Connect4UtilityFunction implements GameUtilityFunction {
         }
       }
     }
-    */
-/*  if (cntEmptyCell > 0) {
+    /*  if (cntEmptyCell > 0) {
       log.info("mon0 ");
       Board board = new Board();
       board.setBoard(boards);
       board.printBoard();
-    }*//*
-
+    }*/
 
     return cntEmptyCell > 0 ? 0 : 1;
   }
 }
-*/
