9745e028cf44a8829ddcc79c1bde8504cf1840e6
mozammal <mozammaltomal.1001@gmail.com>
2020-08-01 02:38:10 +0200

    add more than one depth for alpha beta pruning

--- src/main/java/com/mozammal/connectfourserver/tictactoe/AlphaBetaAlgorithm.java
+++ src/main/java/com/mozammal/connectfourserver/tictactoe/AlphaBetaAlgorithm.java
@@ -22,7 +22,7 @@ public class AlphaBetaAlgorithm implements Algorithm {
 
   @Override
   public int execute() {
-    List<Integer> triedDepths = Arrays.asList(1, 4, 8);
+    List<Integer> triedDepths = Arrays.asList(1, 2, 3, 4, 6, 8);
     int mxExpectation = Integer.MIN_VALUE;
     for (int depth : triedDepths)
       mxExpectation =
