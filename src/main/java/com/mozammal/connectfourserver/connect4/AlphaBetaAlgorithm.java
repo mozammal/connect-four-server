9d64999be3d1fb0c8ec29dace4ee62f83ed08f29
mozammal <mozammaltomal.1001@gmail.com>
2020-08-06 22:25:42 +0200

    polish

--- src/main/java/com/mozammal/connectfourserver/connect4/AlphaBetaAlgorithm.java
+++ src/main/java/com/mozammal/connectfourserver/connect4/AlphaBetaAlgorithm.java
@@ -3,8 +3,7 @@ package com.mozammal.connectfourserver.connect4;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -14,7 +13,6 @@ import static com.mozammal.connectfourserver.config.GameConfig.MINIMUM_SCORE_THR
 @Slf4j
 @Data
 public class AlphaBetaAlgorithm implements Algorithm {
-
 
   private static final int SEARCH_DEPTH = 9;
 
@@ -29,21 +27,26 @@ public class AlphaBetaAlgorithm implements Algorithm {
 
   @Override
   public int execute() {
-    return alphaBetaSearch(Integer.MIN_VALUE, Integer.MAX_VALUE, false, SEARCH_DEPTH);
+    List<Integer> depths = Arrays.asList(2, 8);
+    OptionalInt max =
+        depths.stream()
+            .mapToInt(
+                d -> alphaBetaSearch(Integer.MIN_VALUE, Integer.MAX_VALUE, false, SEARCH_DEPTH))
+            .max();
+    return max.getAsInt();
   }
 
   private int alphaBetaSearch(int alpha, int beta, boolean player, int depth) {
 
     int currentScore = gameUtilityFunction.score();
-    if (currentScore == MAX_SCORE_THRESHOLD
-        || currentScore == MINIMUM_SCORE_THRESHOLD)
+    if (currentScore == MAX_SCORE_THRESHOLD || currentScore == MINIMUM_SCORE_THRESHOLD)
       return currentScore;
 
     if (depth == 0) return currentScore;
     Character[][] board = treeNode.getGameBoard().getBoard();
 
-    for (int i=0; i<board[0].length; i++) {
-      int j = board.length-1;
+    for (int i = 0; i < board[0].length; i++) {
+      int j = board.length - 1;
       for (; j >= 0 && board[j][i] != null; j--) ;
       if (j < 0) continue;
       if (board[j][i] == null) {
